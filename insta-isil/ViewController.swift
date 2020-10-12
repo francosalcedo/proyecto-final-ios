d7f7645f9894c8deac67f4c01c858339e86c470b
Franco Salcedo <frnaco.salcedo.i3@gmail.com>
2020-10-12 03:37:40 -0500

    Advanced

--- insta-isil/ViewController.swift
+++ insta-isil/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var recoveryPasswordButton: UIButton!
+    
+    private let primaryColor = CGColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.00)
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        emailTextField.layer.cornerRadius = 10
+        passwordTextField.layer.cornerRadius = 10
+        loginButton.layer.cornerRadius = 10
+        
+        loginButton.clipsToBounds = true
+
+        emailTextField.clipsToBounds = true
+        emailTextField.layer.borderWidth = 1
+        emailTextField.layer.borderColor = primaryColor
+
+        passwordTextField.clipsToBounds = true
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.layer.borderColor = primaryColor
+
     }
 
+    
 
 }
 
